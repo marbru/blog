76ee69e7f128d1710be8ef3f2ea75a9ca57f7c75
Mar <mar@coconauts.net>
2026-01-29 10:34:42 +0100

    Update url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
 	title: "Bardic Inspiration",
-	url: "https://bardic.coconauts.net",
+	url: "https://mar.coconauts.net/blog/",
 	language: "en",
 	description: "Personal blog of Mar Bartolomé",
 	author: {
 		name: "Mar Bartolomé",
 		email: "mar@coconauts.net",
-		url: "https://bardic.coconauts.net"
+		url: "https://mar.coconauts.net/blog/"
 	}
 }
